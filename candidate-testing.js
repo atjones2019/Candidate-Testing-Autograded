0a712ce9ee5371ecc335b6bbff4c212bf76769ac
atjones2019 <atjones2019@gmail.com>
2024-01-16 17:35:42 -0600

    Trying Part-1

--- candidate-testing.js
+++ candidate-testing.js
@@ -30,7 +30,7 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly / 
   if (candidateAnswer === correctAnswer) {
       console.log("Correct!");
   } else {
